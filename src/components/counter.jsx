eb0771c09a8679859e327b523419475e3db393b2
John Lundgren <john.paul.lundgren@gmail.com>
2019-03-17 10:00:44 +0100

    Added counter component.

---
+++ src/components/counter.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
+
+  //Must be an arrow function to be able to use the this object, otherwise you must bind "this" to the function
+  //in the constructor e.g this.handleIncrement.bind(this)
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map(t => (
+            <li key={t}>{t}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+}
+
+export default Counter;
