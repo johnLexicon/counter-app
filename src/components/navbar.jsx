73c2de0c8cbc9a5246ffa7a28ebfd89016d83c78
John Lundgren <john.paul.lundgren@gmail.com>
2019-03-17 15:29:07 +0100

    changed navbar component to a Functional component.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="https://www.google.se">
-          Navbar{" "}
-          <span className="badge badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//Stateless functional component
+//The props must be declared as a parameter to the functional component.
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="https://www.google.se">
+        Navbar{" "}
+        <span className="badge badge-secondary">{props.totalCounters}</span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
